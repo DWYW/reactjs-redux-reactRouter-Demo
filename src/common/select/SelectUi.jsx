d8b46a7ed957f3aae8d765f43dfb81ffcfae8309
swt <Wshi@evilveconsulting.com.hk>
2017-04-01 18:53:57 +0800

    .

--- src/common/select/SelectUi.jsx
+++ src/common/select/SelectUi.jsx
@@ -2,73 +2,187 @@ import React from 'react'
 import './SelectUi.less'
 
 export default class SelectUi extends React.Component {
-  static propTypes = {
-    options: React.PropTypes.array,
-    selectedIndex: React.PropTypes.number,
-    callback: React.PropTypes.func
-  }
-
   constructor(props) {
     super(props);
+    this.optionsStyle = null;
+    this.animationTime = 200; //200ms
+    this.selectInit = this.selectInit.bind(this);
+    this.mountOptionsStyle = this.mountOptionsStyle.bind(this);
     this.handleOptionsShow = this.handleOptionsShow.bind(this);
-    this.optionsStyle=null;
-    this.animationTime=200; //200ms
+    this.handleOptionsHide = this.handleOptionsHide.bind(this);
+    this.handleSelected = this.handleSelected.bind(this);
     this.state = {
+      options: [],
+      callback: null,
+      placeholder: null,
+      selectType: 'default',
+      errorType: null,
+      selectUiStyle: {},
       selectedStatus: false,
       selectedValue: null,
-      optionsStyle:null
+      selectedIndex: null,
+      optionsStyle: null
     }
   }
 
   componentDidMount() {
-
+    this.selectInit(this.props);
   }
 
-  componentWillUnmount() {
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      options: nextProps.options
+    })
 
+    if (nextProps.type !== this.props.type) {
+      let selectType = this.state.selectType;
+      if (nextProps.type) {
+        if (nextProps.type === 'default' || nextProps.type === 'primary' || nextProps.type === 'success' || nextProps.type === 'error' || nextProps.type === 'danger') {
+          selectType = nextProps.type;
+        }
+      }
+      this.setState({
+        selectType: selectType
+      })
+    }
+
+    if (nextProps.errorType !== this.props.errorType) {
+      let errorType = this.state.errorType;
+      if (nextProps.errorType && (nextProps.errorType === 'error' || nextProps.errorType === 'danger')) {
+        errorType = nextProps.errorType;
+      }
+      this.setState({
+        errorType: errorType
+      })
+    }
+
+    if (nextProps.selectedIndex !== this.state.selectedIndex) {
+      this.setState({
+        selectedIndex:nextProps.selectedIndex,
+        selectedValue:nextProps.options[nextProps.selectedIndex]
+      })
+    }
+
+
+    // return true;
   }
-  mountOptionsStyle(status){
-    const prefixes=["Webkit","Moz","ms","O",""];
-    if(status==='show'){
-      return JSON.parse("{"+prefixes.map((prefix)=>`\"${prefix}Transform\":\"scaleY(1)\"`).join(",")+"}".toString());
-    }else{
-      return JSON.parse("{"+prefixes.map((prefix)=>`\"${prefix}Transform\":\"scaleY(0)\"`).join(",")+"}".toString())
+
+  selectInit(props) {
+    let options = props.options;
+    let callback = props.callback;
+    let selectStyle = {};
+    let selectType = "default";
+    let errorType = null;
+    props.fontSize ? selectStyle.fontSize = props.fontSize : "";
+    props.width ? selectStyle.width = props.width : "";
+    selectStyle.height = props.height ? props.height : "32px";
+    selectStyle.lineHeight = selectStyle.height;
+    let placeholder = props.placeholder ? props.placeholder : null;
+    if (props.type) {
+      if (props.type === 'default' || props.type === 'primary' || props.type === 'success' || props.type === 'error' || props.type === 'danger') {
+        selectType = props.type;
+      }
+    }
+    if (props.errorType && (props.errorType === 'error' || props.errorType === 'danger')) {
+      errorType = props.errorType;
+    }
+    let selectedIndex = null;
+    let selectedValue = null;
+    if (props.selectedIndex !== undefined) {
+      selectedIndex = props.selectedIndex;
+      selectedValue = props.options[selectedIndex];
+    }
+    this.setState({
+      options: options,
+      callback: callback,
+      placeholder: placeholder,
+      errorType: errorType,
+      selectType: selectType,
+      selectUiStyle: selectStyle,
+      selectedIndex: selectedIndex,
+      selectedValue: selectedValue
+    })
+    // console.log(this.state)
+  }
+
+  mountOptionsStyle(status) {
+    const prefixes = ["Webkit", "Moz", "ms", "O", ""];
+    if (status === 'show') {
+      return JSON.parse("{" + prefixes.map((prefix) => `\"${prefix}Transform\":\"scaleY(1)\"`).join(",") + "}".toString());
+    } else {
+      return JSON.parse("{" + prefixes.map((prefix) => `\"${prefix}Transform\":\"scaleY(0)\"`).join(",") + "}".toString())
     }
   }
+
   handleOptionsShow() {
-    const optionsStyle=this.mountOptionsStyle("show");
+    const optionsStyle = this.mountOptionsStyle("show");
     this.setState({
       selectedStatus: true,
-      optionsStyle:optionsStyle
+      optionsStyle: optionsStyle
     })
   }
-  handleOptionsHide(){
-    const optionsStyle=this.mountOptionsStyle("hide");
+
+  handleOptionsHide() {
+    const optionsStyle = this.mountOptionsStyle("hide");
     this.setState({
       selectedStatus: false,
-      optionsStyle:optionsStyle
+      optionsStyle: optionsStyle
     })
   }
+
+  handleSelected(key, val) {
+    if (val !== this.state.selectedValue) {
+      if (this.state.callback) {
+        this.state.callback({index: key, value: val})
+      }
+      this.setState({
+        selectedIndex: key,
+        selectedValue: val
+      })
+    }
+    this.handleOptionsHide();
+  }
+
   render() {
+    const options = this.state.options;
+    const errorType = this.state.errorType;
+    const placeholder = this.state.placeholder;
     return (
-    <div className="select-ui">
-      <label className="select-select" onClick={(e)=>this.handleOptionsShow(e)}>
-        <div className="select-value">dfdfsdfs</div>
+    <div
+    className={errorType ? `select-ui select-ui-error-${errorType} select-ui-${this.state.selectType}` : `select-ui select-ui-${this.state.selectType}`}
+    style={this.state.selectUiStyle}>
+      <label className={this.state.selectedStatus ? "select-select select-focus" : "select-select"}
+             onClick={(e) => this.handleOptionsShow(e)}>
+        <div className="select-value">{this.state.selectedValue || placeholder || "请选择"}</div>
         <i className="select-icon iconfont icon-traingle-down"></i>
       </label>
-
       <div className="select-options">
         <div className="mark" style={{
           display: this.state.selectedStatus ? "block" : "none"
-        }} onClick={(e)=>this.handleOptionsHide(e)}></div>
+        }} onClick={(e) => this.handleOptionsHide(e)}></div>
         <ul style={this.state.optionsStyle}>
-          <li>222</li>
-          <li>222</li>
-          <li>222</li>
-          <li>222</li>
+          {
+            Object.keys(options).map((key) => {
+              return (
+              <li className={key == this.state.selectedIndex ? "select-option-selected" : ""} key={key}
+                  onClick={() => this.handleSelected(key, options[key])}>{options[key]}</li>
+              )
+            })
+          }
         </ul>
       </div>
     </div>
     )
   }
+}
+SelectUi.propTypes = {
+  type: React.PropTypes.string,
+  errorType: React.PropTypes.string,
+  options: React.PropTypes.array,
+  selectedIndex: React.PropTypes.number,
+  callback: React.PropTypes.func,
+  placeholder: React.PropTypes.string,
+  width: React.PropTypes.string,
+  height: React.PropTypes.string,
+  fontSize: React.PropTypes.string
 }
--- src/components/form/select/MySelect.jsx
+++ src/components/form/select/MySelect.jsx
@@ -1,23 +1,99 @@
 import React from 'react'
 import SelectUi from '../../../common/select/SelectUi.jsx'
+import './MySelect.less'
 export default class MySelect extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.testSelectCallback=this.testSelectCallback.bind(this);
+    this.testSelectTypeCallback=this.testSelectTypeCallback.bind(this);
+    this.testSelectErrorTypeCallback=this.testSelectErrorTypeCallback.bind(this);
+    this.state = {
+      testOptions:["select1","select2","select3","select4"],
+      testSelected:null,
+      testSelectType:'default',
+      testSelectErrorType:null
+    }
   }
 
   componentDidMount() {
-
+    if(this.state.testSelectedIndex!==undefined){
+      this.setState({
+        testSelected:this.state.testOptions[this.state.testSelectedIndex]
+      })
+    }
   }
 
   componentWillUnmount() {
 
   }
 
+  testSelectCallback(res){
+    this.setState({
+      testSelected:res.value
+    })
+  }
+  testSelectTypeCallback(res){
+    this.setState({
+      testSelectType:res.value
+    })
+  }
+  testSelectErrorTypeCallback(res){
+    this.setState({
+      testSelectErrorType:res.value
+    })
+  }
+
   render() {
     return (
     <div>
-      <SelectUi></SelectUi>
+      <div className="pannel">
+        <div className="pannel-title">
+          {`<SelectUi></SelectUi>`}
+        </div>
+        <div className="pannel-content">
+          <p className="bold">attribute:</p>
+          <ul>
+            <li>options={`[option,...]`}, array （必需）</li>
+            <li>type={`{type}`}, string 包含default,primary,success,error,danger (默认default)</li>
+            <li>errorType={`{errorType}`}, string 包含error,danger </li>
+            <li>selectedIndex={`{index}`}, number 被选中的位置，从0开始 </li>
+            <li>placeholder={`{info}`}, string 默认提示信息 </li>
+            <li>width={`{width}`}, string select及option宽度，最小120px </li>
+            <li>height={`{height}`}, string select及option高度 </li>
+            <li>fontSize={`{fontSize}`}, string select及option字体大小 </li>
+            <li>callback={`{callback}`}, function 回调函数,返回结果为{`{index:selected,value:selectedValue}`} </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="pannel">
+        <div className="pannel-title">example</div>
+        <div className="pannel-content">
+          <div className="mgt10px">
+           <span className="mgr20px">
+             type:<SelectUi options={["default","primary","success","error","danger"]}  selectedIndex={0} fontSize="12px" callback={this.testSelectTypeCallback}></SelectUi>
+           </span>
+            <span className="mgr20px">
+             errorType:<SelectUi options={["error","danger"]}  placeholder={"请选择"} fontSize="12px" callback={this.testSelectErrorTypeCallback}></SelectUi>
+           </span>
+          </div>
+          <div className="mgt10px">当前选择的值为：{this.state.testSelected ||"请先选择"}</div>
+          <div className="mgt10px">
+            <SelectUi options={this.state.testOptions} type={this.state.testSelectType} errorType={this.state.testSelectErrorType} placeholder="请先选择" fontSize="12px" selectedIndex={this.state.testSelectedIndex} callback={this.testSelectCallback}></SelectUi>
+          </div>
+
+        </div>
+      </div>
+
+      {/*<div>*/}
+        {/*<input className="input" type="text"/> <br/>*/}
+        {/*<input className="input default theme-default" type="text"/> <br/>*/}
+        {/*<input className="input primary" type="text"/> <br/>*/}
+        {/*<input className="input success theme-success" type="text"/> <br/>*/}
+        {/*<input className="input error theme-error" type="text"/> <br/>*/}
+        {/*<input className="input danger theme-danger" type="text"/>*/}
+      {/*</div>*/}
+      
     </div>
     )
   }
